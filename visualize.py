912e966cdf301b7de3f505533ac5224db5f7d685
John Hughes <jpl.hughes@btinternet.com>
2023-04-29 10:32:58 +0100

    improve viz with colours based on the state

--- visualize.py
+++ visualize.py
@@ -1,24 +1,31 @@
 import argparse
-from dash import Dash, html, dcc
+
 import dash_cytoscape as cyto
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+
 from utils import maybe_load_checkpoint
 
-from dash.dependencies import Input, Output
+state2colour = {
+    "new": "grey",
+    "queued": "yellow",
+    "cancelled": "red",
+    "complete": "green",
+}
+
 
 def NamedDropdown(name, **kwargs):
     return html.Div(
-        style={'margin': '10px 0px'},
+        style={"margin": "10px 0px"},
         children=[
-            html.P(
-                children=f'{name}:',
-                style={'margin-left': '3px'}
-            ),
-
-            dcc.Dropdown(**kwargs)
-        ]
+            html.P(children=f"{name}:", style={"margin-left": "3px"}),
+            dcc.Dropdown(**kwargs),
+        ],
     )
+
+
 def DropdownOptionsList(*args):
-    return [{'label': val.capitalize(), 'value': val} for val in args]
+    return [{"label": val.capitalize(), "value": val} for val in args]
 
 
 def visualize_tree(root):
@@ -26,122 +33,162 @@ def visualize_tree(root):
 
     def generate_elements(node, depth=0, parent_id=None):
         elements = []
+        stylesheet = []
         node_id = f"{depth}-{node.task_id}"
-        elements.append({
-            "data": {
-                "id": node_id,
-                "label": node.task_id,
-                "task_name": node.task_name,
-                "result": node.result,
-                "state": node.state
+        elements.append(
+            {
+                "data": {
+                    "id": node_id,
+                    "label": int(node.task_id) + 1,
+                    "task_name": node.task_name,
+                    "result": node.result,
+                    "state": node.state,
+                }
             }
-        })
+        )
+        stylesheet.append(
+            {
+                "selector": f'node[id = "{node_id}"]',
+                "style": {
+                    "background-color": state2colour[node.state],
+                    "opacity": 0.7,
+                    "label": "data(label)",
+                    "font-size": 20,
+                },
+            }
+        )
+
         if parent_id is not None:
             elements.append({"data": {"source": parent_id, "target": node_id}})
 
         for child in node.children:
-            elements.extend(generate_elements(child, depth + 1, node_id))
+            child_elements, child_stylesheet = generate_elements(
+                child, depth + 1, node_id
+            )
+            elements.extend(child_elements)
+            stylesheet.extend(child_stylesheet)
 
-        return elements
+        return elements, stylesheet
 
-    elements = generate_elements(root)
+    elements, stylesheet = generate_elements(root)
 
-    tab_style = {'height': 'calc(98vh - 80px)'}
-    
+    stylesheet.append(
+        {
+            "selector": ":selected",
+            "style": {
+                "border-width": 2,
+                "border-color": "black",
+                "border-opacity": 1,
+                "opacity": 1,
+                "label": "data(label)",
+                "color": "black",
+                "font-size": 20,
+                "z-index": 9999,
+            },
+        }
+    )
+
+    tab_style = {"height": "calc(98vh - 80px)"}
+
     def text_box_style(height="15%"):
-
         style = {
-            'overflow-y': 'scroll',
-            'height': height,
-            'border': 'thin lightgrey solid',
-            'white-space': 'pre-wrap',
-            'word-wrap': 'break-word',
-            'overflow-x': 'hidden'
+            "overflow-y": "scroll",
+            "height": height,
+            "border": "thin lightgrey solid",
+            "white-space": "pre-wrap",
+            "word-wrap": "break-word",
+            "overflow-x": "hidden",
         }
         return style
 
+    # fmt: off
     app.layout = html.Div([
-        html.Div(className='eight columns', children=[
+        html.Div(className="eight columns", children=[
             cyto.Cytoscape(
-                id='cytoscape',
+                id="cytoscape",
                 elements=elements,
-                # stylesheet=default_stylesheet,
-                layout={'name': 'breadthfirst'},
+                stylesheet=stylesheet,
+                layout={"name": "breadthfirst"},
                 style={
-                    'height': '95vh',
-                    'width': 'calc(100% - 500px)',
-                    'float': 'left'
+                    "height": "95vh",
+                    "width": "calc(100% - 500px)",
+                    "float": "left"
                 },
                 responsive=True
             )
         ]),
-        html.Div(className='four columns', children=[
-            dcc.Tabs(id='tabs', children=[
-                dcc.Tab(label='Content', children=[
+        html.Div(className="four columns", children=[
+            dcc.Tabs(id="tabs", children=[
+                dcc.Tab(label="Content", children=[
                     html.Div(style=tab_style, children=[
-                        html.P('Task:'),
+                        html.P("Task:"),
                         html.Pre(
-                            id='tap-node-task',
-                            style=text_box_style("60px") # 4 lines
+                            id="tap-node-task",
+                            style=text_box_style("60px")  # 4 lines
                         ),
-                        html.P('Result:'),
+                        html.P("Result:"),
                         html.Pre(
-                            id='tap-node-result',
+                            id="tap-node-result",
                             style=text_box_style("45%")
                         ),
-                        html.P('state:'),
+                        html.P("state:"),
                         html.Pre(
-                            id='tap-node-state',
+                            id="tap-node-state",
                             style=text_box_style("20px")
                         ),
-                        html.P('Edge Content:'),
+                        html.P("Edge Content:"),
                         html.Pre(
-                            id='tap-edge-json-output',
-                            style=text_box_style("75px") # 5 lines
+                            id="tap-edge-json-output",
+                            style=text_box_style("75px")  # 5 lines
                         )
                     ])
                 ]),
-                dcc.Tab(label='Control Panel', children=[
+                dcc.Tab(label="Control Panel", children=[
                     NamedDropdown(
-                        name='Layout',
-                        id='dropdown-layout',
+                        name="Layout",
+                        id="dropdown-layout",
                         options=DropdownOptionsList(
-                            'breadthfirst',
-                            'grid',
-                            'circle',
-                            'concentric',
-                            'cose',
+                            "breadthfirst",
+                            "grid",
+                            "circle",
+                            "concentric",
+                            "cose",
                         ),
-                        value='breadthfirst',
+                        value="breadthfirst",
                         clearable=False
                     ),
                 ]),
             ]),
         ])
     ])
+    # fmt: on
 
-    @app.callback(Output('tap-node-task', 'children'), [Input('cytoscape', 'tapNode')])
+    @app.callback(Output("tap-node-task", "children"), [Input("cytoscape", "tapNode")])
     def display_task(data):
         task_name = ""
         if data:
             task_name = data["data"].get("task_name", "")
         return task_name
-    
-    @app.callback(Output('tap-node-result', 'children'), [Input('cytoscape', 'tapNode')])
+
+    @app.callback(
+        Output("tap-node-result", "children"), [Input("cytoscape", "tapNode")]
+    )
     def display_result(data):
         result = ""
         if data:
             result = data["data"].get("result", "")
         return result
-    
-    @app.callback(Output('tap-node-state', 'children'), [Input('cytoscape', 'tapNode')])
+
+    @app.callback(Output("tap-node-state", "children"), [Input("cytoscape", "tapNode")])
     def display_state(data):
         state = ""
         if data:
             state = data["data"].get("state", "")
         return state
 
-    @app.callback(Output('tap-edge-json-output', 'children'), [Input('cytoscape', 'tapEdge')])
+    @app.callback(
+        Output("tap-edge-json-output", "children"), [Input("cytoscape", "tapEdge")]
+    )
     def display_tap_edge(data):
         if data:
             source_name = data["sourceData"].get("task_name", "")
@@ -150,12 +197,12 @@ def visualize_tree(root):
         else:
             return ""
 
-
-    @app.callback(Output('cytoscape', 'layout'), [Input('dropdown-layout', 'value')])
+    @app.callback(Output("cytoscape", "layout"), [Input("dropdown-layout", "value")])
     def update_cytoscape_layout(layout):
-        return {'name': layout}
-    
+        return {"name": layout}
+
     app.run_server(debug=True)
+
 
 def main():
     parser = argparse.ArgumentParser()
